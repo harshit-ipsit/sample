3b85badd2beef4c37ca1090c7a991e31976ace4c
harshit-ipsit <harshit.ipsit@gmail.com>
2025-06-27 08:29:55 +0530

    Add files via upload

---
+++ streamlit_demo_webapp/pages/2_📝EOD_Reporting.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import streamlit as st
+
+# --- Page Config ---
+st.set_page_config(
+    page_title="EOD Reporting",
+    page_icon=":memo:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# --- Inline Custom CSS ---
+st.markdown("""
+    <style>
+    /* Hide Streamlit Branding */
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    [data-testid="stSidebarNav"] {
+        background-image: url(https://w.amazon.com/rest/wikis/xwiki/s3files/file/logo-generic.png);
+        background-repeat: no-repeat;
+        padding-top: 10px;
+        background-position: 20px 20px;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.title("EOD Reporting")
+
+
